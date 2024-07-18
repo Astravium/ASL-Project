6c8184322dda9f7d982755dcd15e6b623b23cae5
manuelgerardzuccaro <gerard3599@gmail.com>
2024-07-18 22:47:10 +0200

    DAQ - fix : get_next_capture_number
    Ensures that the program continues to number images correctly, even if images already exist in the folder.

--- DataAcquisition/pythonProject/DataAcquisitionProgram.py
+++ DataAcquisition/pythonProject/DataAcquisitionProgram.py
@@ -1,5 +1,6 @@
 ﻿import cv2
 import os
+import re
 
 # Richiedi il nome dell'utente e l'etichetta del carattere
 name = input("Inserisci il tuo nome: ").upper()
@@ -9,17 +10,28 @@ letter = input("Inserisci il carattere della LIS: ").lower()
 dataset_dir = os.path.join("Dataset", letter)
 os.makedirs(dataset_dir, exist_ok=True)
 
+# Funzione per trovare il prossimo numero di cattura disponibile
+def get_next_capture_number(directory, name, letter):
+    existing_files = os.listdir(directory)
+    max_number = 0
+    pattern = re.compile(rf"{letter}_{name}_(\d+)\.jpg")
+    for file in existing_files:
+        match = pattern.match(file)
+        if match:
+            number = int(match.group(1))
+            if number > max_number:
+                max_number = number
+    return max_number + 1
+
 # Numero di cattura
-capture_count = 0
+capture_count = get_next_capture_number(dataset_dir, name, letter)
 
 # Dimensione dell'immagine ritagliata
 crop_size = 64
 
-
 def save_image(image, filename):
     cv2.imwrite(filename, image)
     print(f'Immagine salvata come {filename}')
-
 
 # Apri la webcam
 cap = cv2.VideoCapture(0)
@@ -47,9 +59,9 @@ while True:
 
     key = cv2.waitKey(1) & 0xFF
     if key == ord('c'):
+        filename = os.path.join(dataset_dir, f"{letter}_{name}_{capture_count}.jpg")
+        save_image(crop_img, filename)
         capture_count += 1
-        file_name = os.path.join(dataset_dir, f"{letter}_{name}_{capture_count}.jpg")
-        save_image(crop_img, file_name)
     elif key == ord('q'):
         print("Uscita dal programma.")
         break
